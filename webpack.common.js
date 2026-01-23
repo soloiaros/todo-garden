a2bd9adcfc905c107495f617e0f0ad4994734eef
soloiaros <soloiaros@gmail.com>
2026-01-23 09:22:21 +0300

    Initial configuration This commit includes everything that is required for start-level building and bundling withWebpack.

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/template.html' }),
+  ],
+  modules: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'htnl-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|svg|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|ttf)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  }
+}
