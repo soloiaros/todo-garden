f25bb4cf7f7e0b44aaf2a641fc2043e1eebd9af3
soloiaros <soloiaros@gmail.com>
2026-01-25 22:44:59 +0300

    Create the boards page layout creation The layout is being created through a function in a separate module

---
+++ src/boards-page-layout.js
@@ -0,0 +1,19 @@
+import './static/styles/boards-screen.css';
+
+export default function(LogicController) {
+  const mainSection = document.querySelector('main');
+
+  const skyDiv = document.createElement('div');
+    
+  const allBoards = LogicController.retrieveBoards();
+  for (let board of allBoards) {
+    const boardDiv = document.createElement('div');
+    boardDiv.classList.add('board-tile');
+    mainSection.appendChild(boardDiv);
+  }
+  const trailingTile = document.createElement('div');
+  trailingTile.classList.add('board-tile', 'trailing-tile');
+  const trailingTileIndex = Math.ceil(Math.random() * 4);
+  mainSection.style.setProperty('--trailing-tile-bg', `url(/images/ground-tiles/tile${trailingTileIndex}.png)`);
+  mainSection.appendChild(trailingTile);
+}
